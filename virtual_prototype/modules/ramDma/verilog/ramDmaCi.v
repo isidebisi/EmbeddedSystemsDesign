b0af70c8940bad42c8852c005fb6f7ad163754b6
Ismael <ismael.frei@epfl.ch>
2024-04-22 13:11:48 +0200

    progress

--- virtual_prototype/modules/ramDma/verilog/ramDmaCi.v
+++ virtual_prototype/modules/ramDma/verilog/ramDmaCi.v
@@ -16,10 +16,13 @@ module ramDmaCi #(parameter [7:0] customId = 8'h00)
 // dma: addressA is operated on by the DMA controller if valueA[12:10]!=3'b000
 
 
-wire cpuEnable <= start & (ciN == customId);
+wire cpuEnable = start & (ciN == customId);
+wire readWriteIn = valueA[9];
+wire [2:0] mode = valueA[12:10];
+wire [3:0] dmaControl = {mode, readWriteIn};
+wire bypassEnable = cpuEnable & (mode == 3'b000);
+wire dmaEnable = cpuEnable & (mode ~= 3'b000);
 
-wire bypassEnable <= cpuEnable & (valueA[12:10] == 3'b000);
-wire dmaEnable <= cpuEnable & (valueA[12:10] ~= 3'b000);
 
 
 reg readOutputReady;
@@ -45,6 +48,12 @@ dualPortSSRAM #(.bitwidth(32), .nrOfEntries(256), .readAfterWrite(0)) ramDmaCi
 
 
 
+/*
+ *
+ * BYPASS MODE, exercise 2.2
+ *
+ */
+
 always @(posedge clock or posedge reset) begin
     if (reset) begin
         readOutputReady <= 0;
@@ -55,13 +64,90 @@ end
 
 
 
-assign addressA <= valueA[8:0];
-assign writeEnableA <= valueA[9] ? bypassEnable : 1'b0;
-assign dataInA <= valueB;
-assign result <= dataOutA ? done : 32'h00000000;
-assign done <=  1'b1 ? (bypassEnable & writeEnableA) :
+assign addressA = valueA[8:0];
+assign writeEnableA = valueA[9] ? bypassEnable : 1'b0;
+assign dataInA = valueB;
+assign result = dataOutA ? done : 32'h00000000;
+assign done =  1'b1 ? (bypassEnable & writeEnableA) :
                 1'b1 ? (bypassEnable & ~writeEnableA & readOutputReady) : 1'b0;
 
+
+
+/*
+ *
+ * DMA MODE, exercise 2.3
+ *
+ */
+
+parameter   RBUSSTADDR     =   4'b0010,
+            WBUSSTADDR     =   4'b0011,
+            RMEMSTADDR     =   4'b0100,
+            WMEMSTADDR     =   4'b0101,
+            RBLOCKSIZE     =   4'b0110,
+            WBLOCKSIZE     =   4'b0111,
+            RBURSTSIZE     =   4'b1000,
+            WBURSTSIZE     =   4'b1001,
+            RSTATREG       =   4'b1010,
+            WSTATREG       =   4'b1011;
+
+
+reg [31:0] busStartAddress, memStartAdress, blockSize, burstSize, statReg;
+wire [31:0] resultDMA;
+
+// block to handle all the control signals from CPU
+
+always @(posedge clock or posedge reset) begin
+    if (reset) begin
+        busStartAddress <= 32'h00000000;
+        memStartAdress <= 32'h00000000;
+        blockSize <= 32'h00000000;
+        burstSize <= 32'h00000000;
+        statReg <= 32'h00000000;
+
+    end else if (dmaEnable) begin
+        case (dmaControl)
+            RBUSSTADDR: begin
+                resultDMA <= busStartAddress;
+            end
+            WBUSSTADDR: begin
+                busStartAddress <= valueB;
+                resultDMA <= valueB;
+            end
+            RMEMSTADDR: begin
+                resultDMA <= memStartAdress;
+            end
+            WMEMSTADDR: begin
+                memStartAdress <= valueB;
+                resultDMA <= valueB;
+            end
+            RBLOCKSIZE: begin
+                resultDMA <= blockSize;
+            end
+            WBLOCKSIZE: begin
+                blockSize <= valueB;
+                resultDMA <= valueB;
+            end
+            RBURSTSIZE: begin
+                resultDMA <= burstSize;
+            end
+            WBURSTSIZE: begin
+                burstSize <= valueB;
+                resultDMA <= valueB;
+            end
+            RSTATREG: begin
+                resultDMA <= statReg;
+            end
+            WSTATREG: begin
+                statReg <= valueB;
+                resultDMA <= valueB;
+            end
+            default: begin
+                resultDMA <= 32'h00000000;
+            end
+            
+        endcase
+    end
+end
 
 
 // values that are not yet used
