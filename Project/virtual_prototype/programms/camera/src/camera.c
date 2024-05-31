c56923506748474fff4402472993417364bfd721
Ismael <ismael.frei@epfl.ch>
2024-05-31 15:04:23 +0200

    first task complete
    sobel completely implemented in software

--- Project/virtual_prototype/programms/camera/src/camera.c
+++ Project/virtual_prototype/programms/camera/src/camera.c
@@ -5,12 +5,12 @@
 #include <floyd_steinberg.h>
 #include <sobel.h>
 
-#define threshold 100
+#define threshold 4000
 
 volatile uint16_t rgb565[640*480];
 volatile uint8_t grayscale[640*480];
-volatile uint8_t floyd[640*480];
-volatile uint8_t previous_floyd[640*480];
+volatile uint8_t sobel[640*480];
+volatile uint8_t previous_sobel[640*480];
 volatile int16_t error_array[642<<1];
 volatile int first_frame = 1; // Flag to indicate if it's the first frame
 
@@ -20,7 +20,9 @@ int main () {
   int reg;
   camParameters camParams;
   vga_clear();
-  
+
+  uint8_t movement_detected_counter = 0;
+
   printf("Initialising camera (this takes up to 3 seconds)!\n" );
   camParams = initOv7670(VGA);
   printf("Done!\n" );
@@ -70,13 +72,13 @@ int main () {
 
     do {
       takeSingleImageBlocking((uint32_t) &grayscale[0]);
-      edgeDetection(grayscale,floyd, camParams.nrOfPixelsPerLine, camParams.nrOfLinesPerImage,128);
+      edgeDetection(grayscale,sobel, camParams.nrOfPixelsPerLine, camParams.nrOfLinesPerImage,128);
       asm volatile ("l.nios_rrr %[out1],r0,%[in2],0x6":[out1]"=r"(result):[in2]"r"(3));
     } while (result != 0);
 
     if (first_frame) {
       for (int i = 0; i < camParams.nrOfPixelsPerLine * camParams.nrOfLinesPerImage; i++) {
-        previous_floyd[i] = floyd[i];
+        previous_sobel[i] = sobel[i];
       }
       first_frame = 0; // Update the flag
       
@@ -85,7 +87,7 @@ int main () {
       int changed_pixels = 0;
       for (int i = 0; i < camParams.nrOfPixelsPerLine * camParams.nrOfLinesPerImage; i++) {
         // Subtract corresponding pixels and check if the difference is significant
-        int diff = floyd[i] - previous_floyd[i];
+        int diff = sobel[i] - previous_sobel[i];
         if (diff != 0) {
           changed_pixels++;
         }
@@ -93,16 +95,16 @@ int main () {
 
       // If there are changed pixels, movement is detected
       if (changed_pixels > threshold) {
-        printf("Movement detected!\n");
+        printf("Movement detected! %d times\n", ++movement_detected_counter);
         // Do something here, e.g., sound an alarm, send a notification, etc.
       }
     }
 
       // Update the previous frame with the current frame
       for (int i = 0; i < camParams.nrOfPixelsPerLine * camParams.nrOfLinesPerImage; i++) {
-        previous_floyd[i] = floyd[i];
+        previous_sobel[i] = sobel[i];
       }
       vga[2] = swap_u32(2);
-      vga[3] = swap_u32((uint32_t) &floyd[0]);
+      vga[3] = swap_u32((uint32_t) &sobel[0]);
   }
 }
